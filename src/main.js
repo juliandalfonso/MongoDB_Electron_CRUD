0dc685a28cd88ddfe2b9b4dc81dbff4465169fa9
julian <juliandavid.alfonso@utp.edu.co>
2021-10-18 21:51:51 -0500

    hello world working

---
+++ src/main.js
@@ -0,0 +1,21 @@
+const {BrowserWindow} = require('electron')
+
+function createWindow()
+{
+    const win = new BrowserWindow(
+        {
+            width:800,
+            height:700,
+            webPreferences:
+            {
+                nodeIntegration:true
+            }
+        }
+    )
+
+    win.loadFile('src/index.html')
+}
+
+
+module.exports = {createWindow}
+
